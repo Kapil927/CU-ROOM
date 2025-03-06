ed8061f409e37e51460d64ee4a01da82bc10dffb
Kapil Thakur <kapilcambridge9@gmail.com>
2025-03-06 18:21:17 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/cu.png",
           },
           variables: {
             colorText: "#fff",
